7669d865faa449d75b7d6e7bf65c7003e735a948
Dariusz <dariusz151@gmail.com>
2018-12-19 11:59:06 +0100

    add condition if deck doesnt have 52 cards

--- WarCardGame/Deck.cs
+++ WarCardGame/Deck.cs
@@ -1,36 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace WarCardGame
 {
     public class Deck
     {
-        private Random _random = new Random();
+        private readonly Random _random = new Random();
         private Stack<Card> _deck = new Stack<Card>();
-        //private readonly string[] _suits = { "Spades", "Clubs", "Hearts", "Diamnods" };
-        
 
+        /// <summary>
+        /// Builds a new deck of 52 cards.
+        /// </summary>
         public void BuildDeck()
         {
+            if (_deck != null && _deck.Count() != 52)
+            {
+                _deck.Clear();
+            }
+            else
+            {
+                Console.WriteLine("A deck with 52 cards already exists.");
+                return;
+            }
+
             foreach (Suit suit in Enum.GetValues(typeof(Suit)))
             {
                 for (int value = 2; value < 16; value++)
                 {
                     Card card = new Card(value, suit);
-                    this._deck.Push(card);
+                    _deck.Push(card);
                 }
             }
         }
 
+        /// <summary>
+        /// Shuffle the deck.
+        /// </summary>
+        public void Shuffle()
+        {
+            _deck = new Stack<Card>(_deck.OrderBy(x => _random.Next()));
+        }
+
+        /// <summary>
+        /// Deal out a card.
+        /// </summary>
+        /// <returns></returns>
+        public Card DealCard()
+        {
+            return _deck.Pop();
+        }
     }
 
-    public enum Suit
-    {
-        Spades,
-        Clubs,
-        Hearts,
-        Diamnods
-    }
+    
 
 }
