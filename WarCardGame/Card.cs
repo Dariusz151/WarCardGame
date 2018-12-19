b518c078ae5ac00c80bd8c189ddc4720a863324f
Dariusz <dariusz151@gmail.com>
2018-12-19 12:02:25 +0100

    add override ToString method

--- WarCardGame/Card.cs
+++ WarCardGame/Card.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace WarCardGame
 {
+    /// <summary>
+    /// Initializes a new instance of the card class.
+    /// </summary>
     public class Card
     {
         public int Value { get; set; }
@@ -13,6 +16,11 @@ namespace WarCardGame
         {
             Suit = suit;
             Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"Card: {this.Value} Suit: {this.Suit.ToString()}.";
         }
     }
 }
