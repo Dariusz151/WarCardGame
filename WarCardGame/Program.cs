97fce36c7d359feacaa13bf4dfa98d15f9d8150c
Dariusz <dariusz151@gmail.com>
2018-12-19 12:05:15 +0100

    added Program.cs method to test if it works

--- WarCardGame/Program.cs
+++ WarCardGame/Program.cs
@@ -2,11 +2,26 @@
 
 namespace WarCardGame
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Card card;
+            Deck deck = new Deck();
+            deck.BuildDeck();
+
+            deck.Shuffle();
+
+            card = deck.DealCard();
+            Console.WriteLine(card);
+            card = deck.DealCard();
+            Console.WriteLine(card);
+            card = deck.DealCard();
+            Console.WriteLine(card);
+            card = deck.DealCard();
+            Console.WriteLine(card);
+
+            Console.ReadKey();
         }
     }
 }
