8e2197ef1949c50705ad9d6e31be66e3bdd15bc5
Dariusz <dariusz151@gmail.com>
2018-12-19 11:29:49 +0100

    add card class

---
+++ WarCardGame/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarCardGame
+{
+    public class Card
+    {
+        public int Value { get; set; }
+        public string Suit { get; set; }
+
+        public Card(int value, string suit)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+    }
+}
